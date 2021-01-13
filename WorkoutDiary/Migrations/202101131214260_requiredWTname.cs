b383499a95fea95b90a66281b061fcffa82d3e45
Ákos Csernik <csernikakos@gmail.com>
2021-01-13 13:48:08 +0100

    modi

---
+++ WorkoutDiary/Migrations/202101131214260_requiredWTname.cs
@@ -0,0 +1,18 @@
+﻿namespace WorkoutDiary.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class requiredWTname : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.WorkoutTypes", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.WorkoutTypes", "Name", c => c.String(maxLength: 255));
+        }
+    }
+}
